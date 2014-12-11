46ebb7d4ad7c59a79a5403e1f239ebf011771b5f
turnburn <bcturner@Brandons-MacBook-Pro.local>
2014-12-11 09:34:34 -0500

    50% done, one class

--- src/orderingGame/OrderingGameForm.java
+++ src/orderingGame/OrderingGameForm.java
@@ -5,17 +5,44 @@
  */
 package orderingGame;
 
+import java.util.Random;
+
 /**
  *
  * @author bcturner
  */
 public class OrderingGameForm extends javax.swing.JFrame {
 
-    /**
-     * Creates new form OrderingGameForm
-     */
+    
     public OrderingGameForm() {
         initComponents();
+        Random gen = new Random();
+        int denominatorNumber = gen.nextInt(9)+4;
+        
+        denominator1.setText(""+denominatorNumber);
+        denominator2.setText(""+denominatorNumber);
+        denominator3.setText(""+denominatorNumber);
+        denominator4.setText(""+denominatorNumber);
+        
+        int numeratorRange = denominatorNumber - 4;
+        
+        int numeratorNumber1 = gen.nextInt(numeratorRange)+1;
+        int numeratorNumber2 = gen.nextInt(numeratorRange)+1;
+        while(numeratorNumber1 == numeratorNumber2){
+            numeratorNumber2 = gen.nextInt(numeratorRange)+1;
+        }
+        int numeratorNumber3 = gen.nextInt(numeratorRange)+1;
+        while(numeratorNumber1 == numeratorNumber2){
+            numeratorNumber2 = gen.nextInt(numeratorRange)+1;
+        }
+        int numeratorNumber4 = gen.nextInt(numeratorRange)+1;
+                
+        numerator1.setText(""+numeratorNumber1);
+        numerator2.setText(""+numeratorNumber2);
+        numerator3.setText(""+numeratorNumber3);
+        numerator4.setText(""+numeratorNumber4);
+        
+       
     }
 
     /**
@@ -27,21 +54,206 @@ public class OrderingGameForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        inOrder = new javax.swing.JButton();
+        userMessage = new javax.swing.JLabel();
+        scoreLabel = new javax.swing.JLabel();
+        scoreNumLabel = new javax.swing.JLabel();
+        backButton = new javax.swing.JLabel();
+        numerator1 = new javax.swing.JLabel();
+        numerator2 = new javax.swing.JLabel();
+        numerator3 = new javax.swing.JLabel();
+        numerator4 = new javax.swing.JLabel();
+        denominator1 = new javax.swing.JLabel();
+        denominator2 = new javax.swing.JLabel();
+        denominator3 = new javax.swing.JLabel();
+        denominator4 = new javax.swing.JLabel();
+        divider1 = new javax.swing.JLabel();
+        divider2 = new javax.swing.JLabel();
+        divider3 = new javax.swing.JLabel();
+        divider4 = new javax.swing.JLabel();
+        blockLabel1 = new javax.swing.JLabel();
+        lArrow1 = new javax.swing.JLabel();
+        rArrow1 = new javax.swing.JLabel();
+        blockLabel2 = new javax.swing.JLabel();
+        rArrow2 = new javax.swing.JLabel();
+        blockLabel3 = new javax.swing.JLabel();
+        lArrow3 = new javax.swing.JLabel();
+        blockLabel4 = new javax.swing.JLabel();
+        lArrow4 = new javax.swing.JLabel();
+        rArrow4 = new javax.swing.JLabel();
+        background = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(new java.awt.Dimension(660, 401));
+        getContentPane().setLayout(null);
+
+        inOrder.setText("Done!");
+        inOrder.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                inOrderMouseClicked(evt);
+            }
+        });
+        getContentPane().add(inOrder);
+        inOrder.setBounds(480, 350, 81, 29);
+        getContentPane().add(userMessage);
+        userMessage.setBounds(280, 40, 0, 0);
+
+        scoreLabel.setText("Score :");
+        getContentPane().add(scoreLabel);
+        scoreLabel.setBounds(480, 20, 42, 16);
+
+        scoreNumLabel.setText("0");
+        getContentPane().add(scoreNumLabel);
+        scoreNumLabel.setBounds(530, 20, 8, 16);
+
+        backButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/back.png"))); // NOI18N
+        getContentPane().add(backButton);
+        backButton.setBounds(10, 10, 70, 53);
+        getContentPane().add(numerator1);
+        numerator1.setBounds(50, 160, 50, 20);
+        getContentPane().add(numerator2);
+        numerator2.setBounds(350, 160, 50, 20);
+        getContentPane().add(numerator3);
+        numerator3.setBounds(200, 160, 50, 20);
+        getContentPane().add(numerator4);
+        numerator4.setBounds(500, 160, 50, 20);
+
+        denominator1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(denominator1);
+        denominator1.setBounds(50, 200, 50, 20);
+
+        denominator2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(denominator2);
+        denominator2.setBounds(200, 200, 50, 20);
+
+        denominator3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(denominator3);
+        denominator3.setBounds(350, 200, 50, 20);
+
+        denominator4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(denominator4);
+        denominator4.setBounds(500, 200, 50, 20);
+
+        divider1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/divider.png"))); // NOI18N
+        getContentPane().add(divider1);
+        divider1.setBounds(490, 180, 70, 18);
+
+        divider2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/divider.png"))); // NOI18N
+        getContentPane().add(divider2);
+        divider2.setBounds(40, 180, 70, 18);
+
+        divider3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/divider.png"))); // NOI18N
+        getContentPane().add(divider3);
+        divider3.setBounds(190, 180, 70, 18);
+
+        divider4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/divider.png"))); // NOI18N
+        getContentPane().add(divider4);
+        divider4.setBounds(340, 180, 70, 18);
+
+        blockLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/blockPanel.png"))); // NOI18N
+        getContentPane().add(blockLabel1);
+        blockLabel1.setBounds(10, 110, 140, 150);
+
+        lArrow1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/leftArrow.png"))); // NOI18N
+        lArrow1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lArrow1MouseClicked(evt);
+            }
+        });
+        getContentPane().add(lArrow1);
+        lArrow1.setBounds(460, 260, 60, 50);
+
+        rArrow1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/rightArrow.png"))); // NOI18N
+        rArrow1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                rArrow1MouseClicked(evt);
+            }
+        });
+        getContentPane().add(rArrow1);
+        rArrow1.setBounds(380, 260, 60, 50);
+
+        blockLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/blockPanel.png"))); // NOI18N
+        getContentPane().add(blockLabel2);
+        blockLabel2.setBounds(160, 110, 140, 150);
+
+        rArrow2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/rightArrow.png"))); // NOI18N
+        rArrow2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                rArrow2MouseClicked(evt);
+            }
+        });
+        getContentPane().add(rArrow2);
+        rArrow2.setBounds(80, 260, 60, 50);
+
+        blockLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/blockPanel.png"))); // NOI18N
+        getContentPane().add(blockLabel3);
+        blockLabel3.setBounds(310, 110, 140, 150);
+
+        lArrow3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/leftArrow.png"))); // NOI18N
+        lArrow3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lArrow3MouseClicked(evt);
+            }
+        });
+        getContentPane().add(lArrow3);
+        lArrow3.setBounds(160, 260, 60, 50);
+
+        blockLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/blockPanel.png"))); // NOI18N
+        getContentPane().add(blockLabel4);
+        blockLabel4.setBounds(460, 110, 140, 150);
+
+        lArrow4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/leftArrow.png"))); // NOI18N
+        lArrow4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lArrow4MouseClicked(evt);
+            }
+        });
+        getContentPane().add(lArrow4);
+        lArrow4.setBounds(310, 260, 60, 50);
+
+        rArrow4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/rightArrow.png"))); // NOI18N
+        rArrow4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                rArrow4MouseClicked(evt);
+            }
+        });
+        getContentPane().add(rArrow4);
+        rArrow4.setBounds(230, 260, 60, 50);
+
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/background.png"))); // NOI18N
+        getContentPane().add(background);
+        background.setBounds(0, 0, 609, 401);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rArrow2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rArrow2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rArrow2MouseClicked
+
+    private void lArrow3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lArrow3MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_lArrow3MouseClicked
+
+    private void rArrow4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rArrow4MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rArrow4MouseClicked
+
+    private void lArrow4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lArrow4MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_lArrow4MouseClicked
+
+    private void rArrow1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rArrow1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rArrow1MouseClicked
+
+    private void lArrow1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lArrow1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_lArrow1MouseClicked
+
+    private void inOrderMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_inOrderMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inOrderMouseClicked
 
     /**
      * @param args the command line arguments
@@ -79,5 +291,33 @@ public class OrderingGameForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel backButton;
+    private javax.swing.JLabel background;
+    private javax.swing.JLabel blockLabel1;
+    private javax.swing.JLabel blockLabel2;
+    private javax.swing.JLabel blockLabel3;
+    private javax.swing.JLabel blockLabel4;
+    private javax.swing.JLabel denominator1;
+    private javax.swing.JLabel denominator2;
+    private javax.swing.JLabel denominator3;
+    private javax.swing.JLabel denominator4;
+    private javax.swing.JLabel divider1;
+    private javax.swing.JLabel divider2;
+    private javax.swing.JLabel divider3;
+    private javax.swing.JLabel divider4;
+    private javax.swing.JButton inOrder;
+    private javax.swing.JLabel lArrow1;
+    private javax.swing.JLabel lArrow3;
+    private javax.swing.JLabel lArrow4;
+    private javax.swing.JLabel numerator1;
+    private javax.swing.JLabel numerator2;
+    private javax.swing.JLabel numerator3;
+    private javax.swing.JLabel numerator4;
+    private javax.swing.JLabel rArrow1;
+    private javax.swing.JLabel rArrow2;
+    private javax.swing.JLabel rArrow4;
+    private javax.swing.JLabel scoreLabel;
+    private javax.swing.JLabel scoreNumLabel;
+    private javax.swing.JLabel userMessage;
     // End of variables declaration//GEN-END:variables
 }
